7b096daa58da672fe25c08a761203b0311f212a0
tony-guillot <tony.guilot@laplateforme.io>
2021-11-03 16:43:55 +0100

    add job05

---
+++ jour02/job05/index.php
@@ -0,0 +1,18 @@
+<?php
+
+ $num= 2; // 2 est le plus petit nombre premier
+
+ while($num<= 1000){
+         $count=0;     
+    for( $i = 1; $i <= $num; $i++){
+        if(($num % $i)==0){ // si la division des deux variable = 0 alors = pas nombre premier count+
+
+         $count++;
+         }
+     } if ($count < 3){ // si la valeur de count est inferieur à 3 c'est un chiffre premier alors    echo $num 
+
+        echo $num . " <br />";
+     }
+     $num++;
+ }  
+
