0b403227e1ddc04b2931d88cd9367964c9e69b3c
tony-guillot <tony.guilot@laplateforme.io>
2021-11-04 13:49:00 +0100

    add jour03

---
+++ jour03/job01/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$tab = [200, 2004, 173, 98, 171, 404, 459];
+
+foreach( $tab as $value){ // $value 
+    
+    if ( $value %  2 == 0){
+
+        echo  $value . "est  paire <br />";
+
+    }elseif ( $value % 2 != 0 ){
+
+        echo  $value . "est  impaire <br />";
+
+    }
+    
+   
+}
