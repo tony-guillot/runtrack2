b76da116765eeef4b7f7c48b972ffb823c9bc949
tony-guillot <tony.guillot@laplateforme.io>
2021-11-07 14:32:06 +0100

    Job05

---
+++ jour04/Job05/index.php
@@ -0,0 +1,30 @@
+
+
+
+
+<form action="" method="post">
+<label for="username">username :</label>
+<input type="text"name="username">
+<label for="passeword">password : </label>
+<input type="text" name="password">
+<input type="submit" name='valider'>
+</form>
+
+<?php 
+
+
+
+
+if($_POST['username'] == 'John'){
+
+
+    if($_POST['password'] == 'Rambo')
+
+        echo 'C\'est pas ma guerre';
+    
+    else{
+        echo 'Votre pire cauchemar';
+    }
+}
+
+?>
