609b2920dfb2c8af568206e721fc11b20b740068
tony-guillot <tony.guillot@laplateforme.io>
2021-11-07 12:53:38 +0100

    jour04 job01

---
+++ jour04/job01/index.php
@@ -0,0 +1,40 @@
+
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="get">
+
+    <input type="text" name="nom">
+    <input type="text" name="prenom">
+    <input type="date" name="date" >
+    <input type="submit" value='valider'>
+    
+
+
+
+
+
+    </form>
+</body>
+</html>
+
+<?php
+
+foreach( $_GET as $val){
+   
+    echo $val . '<br>';
+}
+
+
+
+
+
+?>
