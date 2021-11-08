893a2820208a235148d6ecb019f5bcdd8e33fd6e
tony-guillot <tony.guilot@laplateforme.io>
2021-11-08 11:33:33 +0100

    job04

---
+++ jour05/job04/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$a = 12;
+$b= 15;
+$operation = "add";
+
+
+function calcule($a,$b, $operation){
+
+        if($operation == "add")
+        return ($a + $b);
+
+        
+}
+
+$calcule = calcule($a, $b, $operation);
+
+echo $calcule;
+
+
+
+
